2a617e5b6ab3b9bd51addea91921844999c2c0ab
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2024-08-19 20:29:59 +0300

    feat: Create ui

---
+++ password_app/src/components/Manager.jsx
@@ -0,0 +1,69 @@
+// import { useRef } from 'react';
+
+const Manager = () => {
+  // const ref = useRef();
+  // const showPassword = () => {
+  //   alert("show the password");
+  //   console.log(ref.current.src);
+  //   if (ref.current.src.includes("icons/eyecross.png")) {
+  //     ref.current.src = "icons/eye.png";
+  //   } else {
+  //     ref.current.src = "icons/eyecross.png";
+  //   }
+  // };
+
+  return (
+    <>
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+
+      <div className="mycontainer">
+        <h1 className="font-bold text-purple-200 text-2xl text-center">
+          PassApp
+        </h1>
+        <p className="text-gray-400 text-center">
+          Keep Your Passwords Safe With Our App
+        </p>
+
+        <div className="text-gray-300 flex flex-col p-4 gap-8 justify-center items-center">
+          <input
+            placeholder="Enter website URL"
+            className="rounded-full border border-gray-300 w-full p-4 py-1 bg-gray-800 text-white"
+            type="text"
+            name=""
+            id=""
+          />
+          <div className="flex w-full justify-between gap-8">
+            <input
+              placeholder="Enter Username"
+              className="rounded-full border border-gray-300 w-full p-4 py-1 bg-gray-800 text-white"
+              type="text"
+              name=""
+              id=""
+            />
+            <div className="relative">
+              <input
+                placeholder="Enter Password"
+                className="rounded-full border border-gray-300 w-full p-4 py-1 bg-gray-800 text-white"
+                type="text"
+                name=""
+                id=""
+              />
+              <span className="absolute right-[3px] top-[4px] cursor-pointer">
+                <img className="p-1" width={26} src="icons/eye.png" alt="eye" />
+              </span>
+            </div>
+          </div>
+          <button className="flex justify-center text-white items-center gap-4 bg-purple-600 hover:bg-purple-500 rounded-full px-8 py-2 w-fit border-1 border-purple-700">
+            <lord-icon
+              src="https://cdn.lordicon.com/jgnvfzqg.json"
+              trigger="hover"
+            ></lord-icon>
+            Add Password
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Manager;
---
+++ password_app/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+const Navbar = () => {
+  return (
+    <nav className="bg-gradient-to-b from-purple-900 via-indigo-900 to-black ">
+      <div className="mycontainer flex justify-between items-center px-4 py-5 h-16">
+        <div className="logo font-bold text-purple-200">
+          PassApp
+        </div>
+
+        <ul>
+          <li className="flex gap-4">
+            <a className="hover:text-purple-300 text-purple-200" href="/">
+              Home
+            </a>
+            <a className="hover:text-purple-300 text-purple-200" href="#">
+              About
+            </a>
+            <a className="hover:text-purple-300 text-purple-200" href="#">
+              Contract
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
