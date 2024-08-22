e34bfc241b7093888cd79ea392190d0cfa88b25b
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2024-08-22 12:34:20 +0300

    feat: Connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+require('dotenv').config()
+console.log(process.env.MONGO_URI)
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
